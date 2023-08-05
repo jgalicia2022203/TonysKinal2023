85b3da83a5cb655016b191777bb94759ac968959
jgalicia2022203 <jgalicia-2022203@kinal.edu.gt>
2023-08-04 18:29:17 -0600

    Cambio de controladores y vistas agregado de APIS y demas

--- TonnysKinal2022203/src/org/juangalicia/controller/CompanyController.java
+++ TonnysKinal2022203/src/org/juangalicia/controller/CompanyController.java
@@ -2,6 +2,11 @@ package org.juangalicia.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import org.juangalicia.bean.Company;
+import org.juangalicia.db.Conexion; 
+import org.juangalicia.main.Principal;
+import org.juangalicia.report.GenerateReport;
+import org.controlsfx.control.Notifications;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,9 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-import org.juangalicia.bean.Company;
-import org.juangalicia.db.Conexion; 
-import org.juangalicia.main.Principal;
+import java.util.Optional;
 import javafx.collections.FXCollections; 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -19,29 +22,23 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import org.juangalicia.report.GenerateReport;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import java.util.Optional;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
 
 public class CompanyController implements Initializable {
-    private enum operations {
-        CREATE, SAVE, DELETE, UPDATE, CANCEL, NONE
-    };
-    
-    private operations typeOfOperation = operations.NONE;
+   
+    //Global Variables
     private Principal principalStage;
     private ObservableList<Company> companyList;
     private final String Background="/org/juangalicia/image/Report Background.png";   
-    
     
     @FXML
     private AnchorPane companyPane;
@@ -67,28 +64,14 @@ public class CompanyController implements Initializable {
     private TableColumn colCompanyPhone;
     @FXML
     private JFXButton btnCreate;
-    @FXML
-    private JFXButton btnRead;
-    @FXML
-    private JFXButton btnUpdate;
-    @FXML
-    private JFXButton btnDelete;
-    @FXML
-    private JFXButton btnGeneralReport;
-    @FXML
-    private ImageView imgCreate;
-    @FXML
-    private ImageView imgRead;
-    @FXML
-    private ImageView imgUpdate;
-    @FXML
-    private ImageView imgDelete;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loadData();
+        unlockControls();
     }
- 
+    
+    //Set data on the table view
     public void loadData() {
         tblCompanys.setItems(getCompany());
         colCompanyId.setCellValueFactory(new PropertyValueFactory<Company, Integer>("codeCompany"));
@@ -96,7 +79,16 @@ public class CompanyController implements Initializable {
         colCompanyAdress.setCellValueFactory(new PropertyValueFactory<Company, String>("adress"));
         colCompanyPhone.setCellValueFactory(new PropertyValueFactory<Company, String>("phone")); 
     }
+    
+    //Put info from TableView on the TextFields
+    public void selectElement() {
+        txtCompanyId.setText(String.valueOf(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getCodeCompany()));
+        txtCompanyName.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getNameCompany());
+        txtCompanyAdress.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getAdress());
+        txtCompanyPhone.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getPhone());
+    }
 
+    //Calls the SQL Storage Procedure and almacenates the registers on a ArrayList
     public ObservableList<Company> getCompany() {
         ArrayList<Company> list = new ArrayList<Company>();
         try {
@@ -115,47 +107,42 @@ public class CompanyController implements Initializable {
         return companyList = FXCollections.observableArrayList(list);
     }
 
+    //Main add method
     public void create() {
-        switch (typeOfOperation) {
-            case NONE:
-                clearControls();
-                unlockControls();
-                btnCreate.setText("Save");
-                btnUpdate.setText("Cancel");
-                btnDelete.setDisable(true);
-                btnRead.setDisable(true);
-                btnGeneralReport.setDisable(true);
-                imgCreate.setImage(new Image("/org/juangalicia/image/save.png"));
-                imgUpdate.setImage(new Image("/org/juangalicia/image/cancel.png"));
-                typeOfOperation = operations.SAVE;
-                loadData();
-                break;
-
-            case SAVE:
-                save();
-                clearControls();
-                lockControls();
-                btnCreate.setText("Create Company");
-                btnUpdate.setText("Update Company");
-                btnDelete.setDisable(false);
-                btnRead.setDisable(false);
-                btnGeneralReport.setDisable(false);
-                imgCreate.setImage(new Image("/org/juangalicia/image/create.png"));
-                imgUpdate.setImage(new Image("/org/juangalicia/image/update.png"));
-                typeOfOperation = operations.NONE;
-                loadData();
-                break;
-        }
+        btnCreate.setOnAction(event -> {
+            if (isFormValid()) {
+                if (isDataExistsInTableView(txtCompanyName.getText())) {
+                    notification("Warning", "org/juangalicia/image/warning.png", "The Company already exists in database", 5);
+                    clearControls();
+                } else {
+                    if (showConfirmationDialog("Save Company", "You want to add this company?", 
+                            "Choose your option.", "Save", "Cancel")){
+                        try {
+                            save();
+                            loadData();
+                            clearControls();
+                            notification("Company Registred", "org/juangalicia/image/success.png", "Company added successfully", 5);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        loadData();
+                    }
+                }
+            } else {
+                notification("Error", "org/juangalicia/image/error.png", "please complete all the fields", 5);
+            }
+        });
     }
-
+    
+    //Get the TextField info, almacenates on the bean class variables trought ObservableList and calls SQL Storage Procedure.
     public void save() {
         Company register = new Company();
         register.setNameCompany(txtCompanyName.getText());
         register.setAdress(txtCompanyAdress.getText());
         register.setPhone(txtCompanyPhone.getText());
         try {
-            PreparedStatement procedure = Conexion.getInsance().getConexion()
-                    .prepareCall("call sp_CreateCompany(?,?,?)");
+            PreparedStatement procedure = Conexion.getInsance().getConexion().prepareCall("call sp_CreateCompany(?,?,?)");
             procedure.setString(1, register.getNameCompany());
             procedure.setString(2, register.getAdress());
             procedure.setString(3, register.getPhone());
@@ -165,60 +152,34 @@ public class CompanyController implements Initializable {
             e.printStackTrace();
         }
     }
-
+    
+    //Main update method
     public void edit() {
-        switch (typeOfOperation) {
-            case NONE:
                 if (tblCompanys.getSelectionModel().getSelectedItem() != null) {
-                    btnCreate.setDisable(true);
-                    btnRead.setDisable(true);
-                    btnGeneralReport.setDisable(true);
-                    btnUpdate.setText("Update");
-                    btnDelete.setText("Cancel");
-                    imgUpdate.setImage(new Image("/org/juangalicia/image/update.png"));
-                    imgDelete.setImage(new Image("/org/juangalicia/image/cancel.png"));
-                    unlockControls();
-                    typeOfOperation = operations.UPDATE;
-                } else {
-                    showAlert(Alert.AlertType.ERROR, "No element selected", null, "Please select an element");
+                    if (showConfirmationDialog("Update Company", "You want to modify this company?", 
+                            "Choose your option.", "Update", "Cancel")) {
+                            try {
+                                update();
+                                loadData();
+                                clearControls();
+                                notification("Company Updated", "org/juangalicia/image/success.png", "Company successfully updated from database", 5);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else {
+                            loadData();
+                        }      
                 }
-                break;
-
-            case SAVE:
-                clearControls();
-                lockControls();
-                btnCreate.setText("Create Company");
-                btnUpdate.setText("Update Company");
-                btnDelete.setDisable(false);
-                btnRead.setDisable(false);
-                btnGeneralReport.setDisable(false);
-                imgCreate.setImage(new Image("/org/juangalicia/image/create.png"));
-                imgUpdate.setImage(new Image("org/juangalicia/image/update.png"));
-                typeOfOperation = operations.NONE;
-                loadData();
-                break;
-
-            case UPDATE:
-                update();
-                clearControls();
-                lockControls();
-                btnCreate.setDisable(false);
-                btnRead.setDisable(false);
-                btnGeneralReport.setDisable(false);
-                btnUpdate.setText("Update Company");
-                btnDelete.setText("Delete Company");
-                imgUpdate.setImage(new Image("/org/juangalicia/image/update.png"));
-                imgDelete.setImage(new Image("/org/juangalicia/image/delete.png"));
-                loadData();
-                typeOfOperation = operations.NONE;
-                break;
-        }
+                else {
+                    notification("No element selected", "org/juangalicia/image/error.png", "Please select an element from the table", 5);
+                }
     }
 
+    //Calls SQL Storage Procedure and extracts the info on the variables to set them on the procedure.
     public void update() {
         try {
-            PreparedStatement procedure = Conexion.getInsance().getConexion()
-                    .prepareCall("call sp_UpdateCompany(?,?,?,?)");
+            PreparedStatement procedure = Conexion.getInsance().getConexion().prepareCall("call sp_UpdateCompany(?,?,?,?)");
             Company register = (Company) tblCompanys.getSelectionModel().getSelectedItem();
             register.setNameCompany(txtCompanyName.getText());
             register.setAdress(txtCompanyAdress.getText());
@@ -233,72 +194,31 @@ public class CompanyController implements Initializable {
         }
     }
 
+    //Calls SQL Storage Procedure and set the ID from the TableView selected register on the procedure.
     public void delete() {
-        switch (typeOfOperation) {
-            case UPDATE:
-                update();
-                clearControls();
-                lockControls();
-                btnCreate.setDisable(false);
-                btnRead.setDisable(false);
-                btnGeneralReport.setDisable(false);
-                btnUpdate.setText("Update Company");
-                btnDelete.setText("Delete Company");
-                imgUpdate.setImage(new Image("/org/juangalicia/image/update.png"));
-                imgDelete.setImage(new Image("/org/juangalicia/image/delete.png"));
-                loadData();
-                typeOfOperation = operations.NONE;
-                break;
-
-            default:
                 if (tblCompanys.getSelectionModel().getSelectedItem() != null) {
-                    Alert alert = new Alert(AlertType.CONFIRMATION);
-                    alert.setTitle("Delete Company");
-                    alert.setHeaderText("Are you sure of deleting this register? You are gonna delete a foreign key");
-                    alert.setContentText("Choose your option.");
-
-                    ButtonType buttonTypeYes = new ButtonType("Yes");
-                    ButtonType buttonTypeNo = new ButtonType("No");
-
-                    alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
-
-                    Optional<ButtonType> result = alert.showAndWait();
-                    if (result.isPresent() && result.get() == buttonTypeYes) {
+                    if (showConfirmationDialog("Delete Company", "Are you sure of deleting this register?", 
+                            "Choose your option.", "Delete", "Cancel")){
                         try {
-                            PreparedStatement procedure = Conexion.getInsance().getConexion()
-                                    .prepareCall("call sp_DeleteCompany(?)");
-                            procedure.setInt(1,
-                                    ((Company) tblCompanys.getSelectionModel().getSelectedItem()).getCodeCompany());
+                            PreparedStatement procedure = Conexion.getInsance().getConexion().prepareCall("call sp_DeleteCompany(?)");
+                            procedure.setInt(1,((Company) tblCompanys.getSelectionModel().getSelectedItem()).getCodeCompany());
                             procedure.execute();
                             companyList.remove(tblCompanys.getSelectionModel().getSelectedItem());
                             clearControls();
+                            notification("Company Deleted", "org/juangalicia/image/success.png", 
+                                    "Company successfully deleted from database", 5);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
                 } else {
-                    Alert alert = new Alert(AlertType.WARNING);
-                    alert.setTitle("Warning");
-                    alert.setHeaderText("You should select an element");
-                    alert.showAndWait();
+                    notification("No element selected", "org/juangalicia/image/error.png", "Please select an element from the table", 5);
                 }
-        }
     }
 
-    public void selectElement() {
-        txtCompanyId.setText(
-                String.valueOf(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getCodeCompany()));
-        txtCompanyName.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getNameCompany());
-        txtCompanyAdress.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getAdress());
-        txtCompanyPhone.setText(((Company) tblCompanys.getSelectionModel().getSelectedItem()).getPhone());
-    }
-
+    //Report statements
     public void generateReport() {
-        switch (typeOfOperation) {
-            case NONE:
                 printReport();
-                break;
-        }
     }
 
     public void printReport() {
@@ -321,11 +241,12 @@ public class CompanyController implements Initializable {
             parameters.put("codeCompany", codeCompany);
             parameters.put("fondo", Background);
             GenerateReport.showReport("GeneralReport.jasper", "General Report", parameters);
-        }catch(Exception e){
+        } catch(Exception e){
             e.printStackTrace();
         }
     }
     
+    //FiltredList for the search TextField
     public void companySearch() {
 
         FilteredList<Company> filter = new FilteredList<>(companyList, e -> true);
@@ -360,15 +281,7 @@ public class CompanyController implements Initializable {
         tblCompanys.setItems(sortList);
     }
     
-    public void close() {
-        System.exit(0);
-    }
-    
-    public void minimize() {
-        Stage stage = (Stage) companyPane.getScene().getWindow();
-        stage.setIconified(true);
-    }
-    
+    //Menubar buttons methods
     public void logout() {
        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
        alert.setTitle("Logout");
@@ -387,22 +300,67 @@ public class CompanyController implements Initializable {
                         }
                     }
                     else {
-                        principalStage.principalWindow();
-                }
+                        principalStage.companyWindow();
+                    }
     }
-
-    private void showAlert(Alert.AlertType alertType, String title, String header, String content) {
-        Alert alert = new Alert(alertType);
+    
+    public void minimize() {
+        Stage stage = (Stage) companyPane.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+    //Notifications and dialogs methods
+    public void notification(String title, String img, String text, int seconds){
+        Image imgN = new Image(img);
+        Notifications notification = Notifications.create();
+        notification.title(title);
+        notification.graphic(new ImageView(imgN));
+        notification.text(text);
+        notification.hideAfter(Duration.seconds(seconds));
+        notification.position(Pos.BASELINE_RIGHT);
+        notification.show();
+    }
+    
+    private boolean showConfirmationDialog(String title, String header, String content, String button1, String button2) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
-        alert.showAndWait();
+        ButtonType buttonTypeYes = new ButtonType(button1);
+        ButtonType buttonTypeNo = new ButtonType(button2);
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == buttonTypeYes;
+    }
+    
+    //Methods to Search existing registers on the TableView
+    public boolean isDataExistsInTableView(String newData) {
+        ObservableList<Company> items = tblCompanys.getItems();
+        
+        for (Company company : items) {
+            if (company.getNameCompany().equals(newData)) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    private boolean isTextFieldEmpty(JFXTextField textField) {
+        return textField.getText().trim().isEmpty();
+    }
+    
+    private boolean isFormValid() {
+        return !isTextFieldEmpty(txtCompanyName)
+                && !isTextFieldEmpty(txtCompanyAdress)
+                && !isTextFieldEmpty(txtCompanyPhone);
+    }
+
+    //Controls methods
     public void lockControls() {
         txtCompanyId.setEditable(false);
         txtCompanyName.setEditable(false);
         txtCompanyAdress.setEditable(false);
+        txtCompanyPhone.setEditable(false);
     }
 
     public void unlockControls() {
@@ -419,6 +377,7 @@ public class CompanyController implements Initializable {
         txtCompanyPhone.clear();
     }
     
+    //Stages
     public void principalWindow(){
         this.principalStage.principalWindow();
     }
